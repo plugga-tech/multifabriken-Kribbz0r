f36153e3344af6aaef3e4054acd3bca67d8eb54c
Kribbz0r <kribbz0r@gmail.com>
2023-11-13 18:15:06 +0100

    Closes #2. Can we add a real comment after? --test--

---
+++ Multifabriken/src/Car.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Car {
+    // Things cars have
+    private String coulor;
+    private String brand;
+    private String numberPlate;
+
+    // List of cars
+    private ArrayList<Car> cars;
+
+    // constructor for cars.
+    private Car() {
+
+    }
+
+    // method for creating a car. a car has a numberplate, colour and a brand
+
+}
