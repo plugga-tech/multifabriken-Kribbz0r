7f48bba2d5fb7ce8f3d664eab6dfd55c4582b2d2
Kribbz0r <kribbz0r@gmail.com>
2023-11-13 17:34:43 +0100

    Closes #1

--- Multifabriken/src/App.java
+++ Multifabriken/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // method that lists what can be ordered.
+
+        // list the avalible options för the user.
+
+        // Create switch to see what the user wants to do. Check for int 1-6.
+        // option 1, create car method
+        // option 2, create candy method
+        // option 3, create pipes method
+        // option 4, create oat milk method
+        // option 5, print what the user has ordered.
+        // option 6, exit the app
+
     }
 }
